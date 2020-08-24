d819c0427d59ccd1c99def2fb0a7191438adffa2
Engine <Alexey@mbp-engine.mynet>
2020-08-24 20:31:54 +0100

    first project commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import Profile from './Components/Profile';
+
+
+
+const App = () => {
+  return (
+    <div className='app-wrapper'>
+      <Header />
+      <Navbar />
+      <Profile />
+    </div>
+  );
+}
+
+
+
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <header className='header'>
+    <img src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fabali.ru%2Fwp-content%2Fuploads%2F2011%2F01%2FLokomotiv_Moscow-600x720.png&f=1&nofb=1' />
+  </header>
+    );
+}
+
+export default Header;
