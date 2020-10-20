82fb599b13629d6c5a153eb9fb80d02968a6efeb
Engine <Alexey@mbp-engine.mynet>
2020-10-20 16:18:30 +0100

    wrapped state in store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { addPost, addMessage, updateNewPostText, updateNewMessageText, subscribe } from './redux/state';
 import { BrowserRouter } from 'react-router-dom'; 
 import * as serviceWorker from './serviceWorker';
-import state from './redux/state';
+import store from './redux/state';
 
 export let rerenderEntireTree = (state) => {
   ReactDOM.render(
@@ -14,18 +13,18 @@ export let rerenderEntireTree = (state) => {
       <BrowserRouter>
         <App
           state={state}
-          addPost={addPost}
-          addMessage={addMessage}
-          updateNewPostText={updateNewPostText}
-          updateNewMessageText={updateNewMessageText} />
+          addPost={store.addPost.bind(store)}
+          addMessage={store.addMessage}
+          updateNewPostText={store.updateNewPostText.bind(store)}
+          updateNewMessageText={store.updateNewMessageText} />
       </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root'));
-}
+} 
 
-rerenderEntireTree (state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,72 +1,71 @@
-let rerenderEntireTree = () => {
-    console.log('state changed');
-}
-
-let state = {
-    profilePage: {
-        posts: [
-            { id: '1', post: 'Hey! How are you?', likesCount: '59' },
-            { id: '2', post: 'Hey, it is my first post!', likesCount: '11' }
-        ],
-        newPostText: 'type your comment'
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                { id: '1', post: 'Hey! How are you?', likesCount: '59' },
+                { id: '2', post: 'Hey, it is my first post!', likesCount: '11' }
+            ],
+            newPostText: 'type your comment'
+        },
+        messagesPage: {
+            dialogs: [
+                { id: '1', name: 'Alexey' },
+                { id: '2', name: 'Natalie' },
+                { id: '3', name: 'Boris' },
+                { id: '4', name: 'Anna' },
+                { id: '5', name: 'Tanya' }
+            ],
+            messages: [
+                { id: '1', message: 'Hey, how are you?' },
+                { id: '2', message: 'Are you coming today?' },
+                { id: '3', message: 'Can you send me the documents we talked about pls? I will need them tomorrow' }
+            ],
+            newMessageText: 'type your message'
+        },
+        navbar: {
+            friends: [
+                { id: '1', name: 'Anton', ava: "https://vokrug-tv.ru/pic/person/4/5/9/5/4595cbf1bb37fdc619ee8c0d04d05dd7.jpg" },
+                { id: '2', name: 'Dima', ava: "https://cdn.iz.ru/sites/default/files/styles/900x506/public/article-2017-11/RIAN_3173477.HR_.ru_1.jpg?itok=T8jXYstr"},
+                { id: '3', name: 'Charlie', ava: "https://premierliga.ru/netcat_files/3/1/Chorluka-Vedran-55790.jpg"}
+            ]
+        }
     },
-    messagesPage: {
-        dialogs: [
-            { id: '1', name: 'Alexey' },
-            { id: '2', name: 'Natalie' },
-            { id: '3', name: 'Boris' },
-            { id: '4', name: 'Anna' },
-            { id: '5', name: 'Tanya' }
-        ],
-        messages: [
-            { id: '1', message: 'Hey, how are you?' },
-            { id: '2', message: 'Are you coming today?' },
-            { id: '3', message: 'Can you send me the documents we talked about pls? I will need them tomorrow' }
-        ],
-        newMessageText: 'type your message'
+    getState() {
+        return this._state;
     },
-    navbar: {
-        friends: [
-            { id: '1', name: 'Anton', ava: "https://vokrug-tv.ru/pic/person/4/5/9/5/4595cbf1bb37fdc619ee8c0d04d05dd7.jpg" },
-            { id: '2', name: 'Dima', ava: "https://cdn.iz.ru/sites/default/files/styles/900x506/public/article-2017-11/RIAN_3173477.HR_.ru_1.jpg?itok=T8jXYstr"},
-            { id: '3', name: 'Charlie', ava: "https://premierliga.ru/netcat_files/3/1/Chorluka-Vedran-55790.jpg"}
-        ]
-    }
-};
-
-export const addPost = () => {
-    let newPost = {
-        id: 3,
-        post: state.profilePage.newPostText,
-        likesCount: 0
-    }
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
+    _callSubscriber() {
+        console.log('state changed');
+    },
+    addPost() {
+        let newPost = {
+            id: 3,
+            post: this._state.profilePage.newPostText,
+            likesCount: 0
+        }
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    addMessage (newText) {
+        let newMessage = {
+            id: 4, 
+            message: this._state.messagesPage.newMessageText
+        }
+        this._state.messagesPage.messages.push(newMessage);
+        this._state.messagesPage.newMessageText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText (newText) {
+        this._state.messagesPage.newMessageText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer;
+    }    
 }
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const addMessage = (newText) => {
- let newMessage = {
-     id: 4, 
-     message: state.messagesPage.newMessageText
- }
- state.messagesPage.messages.push(newMessage);
- state.messagesPage.newMessageText = '';
- rerenderEntireTree(state);
-}
-
-export const updateNewMessageText = (newText) => {
-    state.messagesPage.newMessageText = newText;
-    rerenderEntireTree(state);
-} 
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
