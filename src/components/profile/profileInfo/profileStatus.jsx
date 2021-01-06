fb301f23994cd78bb08d98e642a51058f0233fc2
Engine <Alexey@mbp-engine.mynet>
2021-01-06 18:15:34 +0000

    added componentDidUpdate to status component to fix status update issue

--- src/components/profile/profileInfo/profileStatus.jsx
+++ src/components/profile/profileInfo/profileStatus.jsx
@@ -28,8 +28,17 @@ class ProfileStatus extends React.Component {
     this.props.updateStatus(this.state.status)
   }
 
-  
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.status !== this.props.status) {
+      this.setState({
+        status: this.props.status
+      })
+    }
+    console.log('componentDidUpdate')
+  }
+
   render() {
+    console.log('render')
     return (
       <div>
         {!this.state.editMode && 
